ca57d5274d1320d1425d4fd4526c0d60240907b8
miludmann <miludmann@5af99735-7728-ca94-68db-61ddb57cc80f>
2011-01-04 23:37:00 +0000

    Working code for the Terrorist and Counter Terrorist Units. Radar is now implemented.

---
+++  legointernational/LegoLab/src/Units/LocalRemote.java
@@ -0,0 +1,37 @@
+package Units;
+
+import lejos.nxt.*; 
+import lejos.nxt.comm.*; 
+import lejos.robotics.navigation.TachoPilot;
+
+import java.io.*; 
+
+public class LocalRemote { 
+  private TachoPilot pilot = new TachoPilot(5.6f,16.0f,Motor.B, Motor.C,true); 
+  private UltrasonicSensor sonic = new UltrasonicSensor(SensorPort.S3); 
+  private Motor motor = new Motor(MotorPort.A);
+  
+  public void run() { 
+    // Create thread, make it a daemon and start it 
+    Thread remote = new Thread(new RemoteControl()); 
+    Thread radar = new Thread(new Radar(motor, sonic));
+    remote.setDaemon(true); 
+    remote.start(); 
+    radar.start();
+    
+    // Drive forward 
+    pilot.forward(); 
+    
+    // Avoid obstacles 
+    while(!Button.ESCAPE.isPressed()) { 
+    } 
+    
+    System.exit(1);
+  } 
+
+  public static void main(String[] args) { 
+    new LocalRemote().run(); 
+  } 
+
+
+}
